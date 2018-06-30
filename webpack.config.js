448db17eccebe7355cceeb67daba92bffc979c6f
Bruemor <arymer1@gmail.com>
2018-06-30 11:55:23 -0400

    Added babelrc file and webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+})
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function() {
+                                return [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        htmlPlugin
+    ]
+}
